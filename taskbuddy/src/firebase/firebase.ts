adbac2826c4456794bcb7152600e3ddecca1f8ae
Sridaran Sivakumar <sridaranus@gmail.com>
2025-02-21 14:25:43 +0530

    Update firebase.ts

--- taskbuddy/src/firebase/firebase.ts
+++ taskbuddy/src/firebase/firebase.ts
@@ -3,13 +3,13 @@ import {initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, getDocs, updateDoc, deleteDoc, doc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBw6oGbjGZcrScfo80prVHvWi_S-qH_j_0",
-  authDomain: "taskbuddy-d67d7.firebaseapp.com",
-  projectId: "taskbuddy-d67d7",
-  storageBucket: "taskbuddy-d67d7.firebasestorage.app",
-  messagingSenderId: "1074758133480",
-  appId: "1:1074758133480:web:bb07c6fd1fe0e709f74b5d",
-  measurementId: "G-FXGTQMNGF4"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
